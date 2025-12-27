6cb93039eeeda2256188d189ad4cfaeb6f362868
qwertyarpit <arpit25122001@gmail.com>
2025-12-27 23:42:11 +0530

    new sensi

--- app/play/page.js
+++ app/play/page.js
@@ -8,10 +8,10 @@ export default function PlayGame() {
   const router = useRouter();
 
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "/game/Build/builddd.loader.js",
-    dataUrl: "/game/Build/builddd.data.unityweb",
-    frameworkUrl: "/game/Build/builddd.framework.js.unityweb",
-    codeUrl: "/game/Build/builddd.wasm.unityweb",
+    loaderUrl: "/game/Build/senddd.loader.js",
+    dataUrl: "/game/Build/senddd.data.unityweb",
+    frameworkUrl: "/game/Build/senddd.framework.js.unityweb",
+    codeUrl: "/game/Build/senddd.wasm.unityweb",
   });
 
   // --- SECURITY HEARTBEAT ---
